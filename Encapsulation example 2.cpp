43637cf933750314cf9acd2157127a23439554ce
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-16 20:30:34 +0500

    Add files via upload

---
+++ Encapsulation example 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class Factorial {
+private:
+    int number;
+
+    // Private method to calculate factorial
+    long long calculateFactorial(int n) {
+        if (n <= 1) return 1;
+        return n * calculateFactorial(n - 1);
+    }
+
+public:
+    // Public method to set the number
+    void setNumber(int num) {
+        number = num;
+    }
+
+    // Public method to get the factorial
+    long long getFactorial() {
+        return calculateFactorial(number);
+    }
+};
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    Factorial factorial;
+    factorial.setNumber(num);
+    std::cout << "Factorial of " << num << " is: " << factorial.getFactorial() << std::endl;
+
+    return 0;
+}
