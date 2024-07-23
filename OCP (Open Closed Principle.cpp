16bf8d5b1565f6f96c2febe592bdd3e6326ce9b9
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-23 18:25:29 +0500

    Add files via upload

---
+++ OCP (Open Closed Principle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+// Base class (closed for modification)
+class Shape {
+public:
+    virtual void draw() const = 0; // Open for extension
+};
+
+// Derived classes (open for extension)
+class Circle : public Shape {
+public:
+    void draw() const override {
+        std::cout << "Drawing Circle" << std::endl;
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    void draw() const override {
+        std::cout << "Drawing Rectangle" << std::endl;
+    }
+};
+
+int main() {
+    Circle circle;
+    Rectangle rectangle;
+
+    Shape* shapes[] = { &circle, &rectangle };
+
+    for (auto shape : shapes) {
+        shape->draw();
+    }
+
+    return 0;
+}
