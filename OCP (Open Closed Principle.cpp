a5a7d09042dc1280d35b42b0129e30304993659b
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-23 18:32:44 +0500

    Update OCP (Open Closed Principle.cpp

--- OCP (Open Closed Principle.cpp
+++ OCP (Open Closed Principle.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
+
 class Shape {
 public:
     virtual void draw() const = 0; // Open for extension
 };
+
 class Circle : public Shape {
 public:
     void draw() const override {
         cout << "Drawing Circle" << endl;
     }
 };
+
 class Rectangle : public Shape {
 public:
     void draw() const override {
@@ -20,7 +23,9 @@ public:
 int main() {
     Circle circle;
     Rectangle rectangle;
+    
     Shape* shapes[] = { &circle, &rectangle };
+    
     for (auto shape : shapes) {
         shape->draw();
     }
