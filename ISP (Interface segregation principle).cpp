729893fcd3fb90284b0ade71e5648b82f45419ea
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-25 18:56:24 +0500

    Add files via upload

---
+++ ISP (Interface segregation principle).cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+// Interface for a document
+class Document {
+public:
+    virtual void open() = 0;
+    virtual void close() = 0;
+    virtual void save() = 0;
+    virtual void print() = 0;
+};
+
+// Class implementing a TextDocument
+class TextDocument : public Document {
+public:
+    void open() override {
+        std::cout << "Opening text document." << std::endl;
+    }
+
+    void close() override {
+        std::cout << "Closing text document." << std::endl;
+    }
+
+    void save() override {
+        std::cout << "Saving text document." << std::endl;
+    }
+
+    void print() override {
+        std::cout << "Printing text document." << std::endl;
+    }
+};
+
+// Interface for a Machine
+class Machine {
+public:
+    virtual void print() = 0;
+};
+
+// Class implementing a PrinterMachine
+class PrinterMachine : public Machine {
+public:
+    void print() override {
+        std::cout << "Printing from the printer." << std::endl;
+    }
+};
+
+// Function demonstrating usage
+void operate(Document& document) {
+    document.open();
+    document.print();
+    document.save();
+    document.close();
+}
+
+int main() {
+    TextDocument textDocument;
+    PrinterMachine printer;
+
+    operate(textDocument);  // Operate on TextDocument
+    // operate(printer);    // Error: PrinterMachine doesn't implement all methods of Document
+
+    return 0;
+}
